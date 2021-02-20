0ece4a23c3aee79303e7af94dc6fba8982a6ffd9
Shreshta <raviakutota7@gmail.com>
2021-02-20 15:29:32 -0500

    first commit

---
+++ src/main/java/com/Shreehan/Application.java
@@ -0,0 +1,13 @@
+package com.Shreehan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
